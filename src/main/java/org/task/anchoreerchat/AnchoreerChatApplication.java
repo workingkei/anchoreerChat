0618fec09f93f6aeb2bdebae697ac3c3a50f8cf7
mskei <kei@test.com>
2024-05-21 10:50:46 +0900

    init

---
+++ src/main/java/org/task/anchoreerchat/AnchoreerChatApplication.java
@@ -0,0 +1,13 @@
+package org.task.anchoreerchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchoreerChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnchoreerChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/task/anchoreerchat/AnchoreerChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.task.anchoreerchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchoreerChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
