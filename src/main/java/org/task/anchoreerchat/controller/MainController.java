2c4bbfb5c4736365d7f8ce69e1491707175d6488
mskei <kei@test.com>
2024-05-22 14:25:30 +0900

    채팅 페이지 적용 및 채팅방 연결

--- src/main/java/org/task/anchoreerchat/controller/MainController.java
+++ src/main/java/org/task/anchoreerchat/controller/MainController.java
@@ -2,7 +2,9 @@ package org.task.anchoreerchat.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -11,5 +13,11 @@ public class MainController {
         @GetMapping("/")
         public String index() {
             return "index";
+        }
+
+        @GetMapping("/chat/{chatRoomId}")
+        public String chat(@PathVariable String chatRoomId, Model model) {
+            model.addAttribute("chatRoomId", chatRoomId);
+            return "chat";
         }
 }
